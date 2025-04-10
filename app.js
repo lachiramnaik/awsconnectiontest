84cb2f5e461a3663f4984987fc7e100e4846c224
lachiramnaik <lmalothu@zilliontechnologies.com>
2025-04-11 00:12:06 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import console from 'console'
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello Malothu Lachiramnaik');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
